872470ca510596ac469bea62da99cc6064606d05
LeandroBarbosaBento <67109002+LeandroBarbosaBento@users.noreply.github.com>
2022-09-01 18:47:17 -0300

    Implementação do corpo dos métodos dos outros endpoints

--- app/Http/Controllers/API/BooksController.php
+++ app/Http/Controllers/API/BooksController.php
@@ -22,7 +22,7 @@ class BooksController extends Controller
      */
     public function index()
     {
-        //
+        return $this->book->get();
     }
 
     /**
@@ -39,34 +39,38 @@ class BooksController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Book $book)
     {
-        //
+        return $book;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Book book
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Book $book)
     {
-        //
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->save();
+
+        return $book;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Book $book
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Book $book)
     {
-        //
+        return $book->delete();
     }
 }
